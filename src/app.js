ff23ff5c78cf2049b55b7073653e79895823699b
AndelijaKaluderovic <andeka011@gmail.com>
2022-02-23 13:15:57 +0100

    converting some class components to functional

--- src/app.js
+++ src/app.js
@@ -54,51 +54,44 @@ class IndecisionApp extends React.Component {
     )
   }
 }
-class Header extends React.Component {
-  render() {
+const Header = (props) => {
     return (
       <div className="header">
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
+        <h1>{props.title}</h1>
+        <h2>{props.subtitle}</h2>
       </div>
     )
   }
-}
-class Action extends React.Component {
-  render() {
+const Action = (props) => {
     return (
       <div className="action">
         <button
-          onClick={this.props.handlePick}
-          disabled={!this.props.hasOptions}
+          onClick={props.handlePick}
+          disabled={!props.hasOptions}
         >
           What should I do?
         </button>
       </div>
     )
   }
-}
-class Options extends React.Component {
-  render() {
+const Options = (props) => {
     return (
       <div className="options">
-        <button onClick={this.props.handleDeleteOptions}>Remove all</button>
+        <button onClick={props.handleDeleteOptions}>Remove all</button>
         {
-          this.props.options.map((option) => <Option key={option} optionText={option} />)
+          props.options.map((option) => <Option key={option} optionText={option} />)
         }
       </div>
     )
   }
-}
-class Option extends React.Component {
-  render() {
+
+const Option = () => {
     return (
       <div className="option">
-        <p>{this.props.optionText}</p>
+        <p>{props.optionText}</p>
       </div>
     )
   }
-}
 class AddOption extends React.Component {
   constructor(props) {
   super(props)
