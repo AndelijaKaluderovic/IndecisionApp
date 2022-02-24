80bed4639c04dea16520ca7580f4735aa7e0bda1
AndelijaKaluderovic <andeka011@gmail.com>
2022-02-24 19:15:11 +0100

    styling done for desktop

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -4,13 +4,20 @@ import Modal from 'react-modal'
 const OptionModal = (props) => (
     <Modal
         isOpen={!!props.selectedOption}
-        contentLabel="Selected Option"
         onRequestClose={props.handleHideModal}
         //onRequest for closing when we click in the background
+        contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleHideModal}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button
+            className="button"
+            onClick={props.handleHideModal}
+        >
+            Okay
+        </button>
     </Modal>
 )
 
