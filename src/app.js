4b06ea46a54d11ca482bc8df7c6a0da9d55593ec
AndelijaKaluderovic <andeka011@gmail.com>
2022-02-25 17:43:01 +0100

    boilerplate

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Indecision from './components/Indecision';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
   
-ReactDOM.render(<Indecision />, document.getElementById('app'));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
